2e272630a5dc1ad38d73eafe86f7dba0d45c29e1
Thomas Haygarth <haygarth.thomas@gmail.com>
2021-01-29 20:18:24 +0000

    Added a basic artifact item type enum and data

---
+++ Assets/Script/Artifacts/ArtifactItemData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtifactItemData : MonoBehaviour
+{
+    [SerializeField]
+    private ArtifactItemType m_ItemType;
+    [SerializeField]
+    private int m_CorrectItemScore = 100;
+    [SerializeField]
+    private int m_IncorrectItemScore = -100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
