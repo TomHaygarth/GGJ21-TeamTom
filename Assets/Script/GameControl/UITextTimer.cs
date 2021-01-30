3dc1e1089ef2ecc9da79cbca80c8bc8b5217f0d7
Thomas Haygarth <haygarth.thomas@gmail.com>
2021-01-30 22:50:14 +0000

    Added some timer controls for the ui

---
+++ Assets/Script/GameControl/UITextTimer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextTimer : TimerController
+{
+    [SerializeField]
+    Text m_text = null;
+
+    // Update is called once per frame
+    new void Update()
+    {
+        base.Update();
+
+        if (m_text != null)
+        {
+            m_text.text = TimeSpan.FromSeconds(currentTime).ToString(@"mm\\:ss");
+        }
+    }
+}
