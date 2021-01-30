e8c4841cf0683d27c9fa11257350f593651434ac
Thomas Haygarth <haygarth.thomas@gmail.com>
2021-01-30 02:47:40 +0000

    added a simple  scriptable object that should allow us to define controls per player easily. I know unity input axis work but they can be annoying to setup at times and this will allow quicker setup for multiple players on keyboards if desired

---
+++ Assets/Script/Input/InputMapping.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "player_controls", menuName = "Data/Input/Input Mapping")]
+public class InputMapping : ScriptableObject
+{
+    public KeyCode UpKey = KeyCode.W;
+    public KeyCode DownKey = KeyCode.S;
+    public KeyCode LeftKey = KeyCode.A;
+    public KeyCode RightKey = KeyCode.D;
+
+    public KeyCode ScanKey = KeyCode.Space;
+    public KeyCode DigKey = KeyCode.E;
+
+    public float DirectionDeadzone = 0.05f;
+}
