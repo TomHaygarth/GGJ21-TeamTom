2c3a32e513536e5006a81e0a0cba566405a9245d
Thomas Haygarth <haygarth.thomas@gmail.com>
2021-01-30 18:19:45 +0000

    added friction zone trigger

---
+++ Assets/Script/Environment/FrictionZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrictionZone : MonoBehaviour
+{
+    [SerializeField]
+    private float m_coefficient = 1.0f;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider col)
+    {
+        PlayerMovementController player = col.GetComponent<PlayerMovementController>();
+        if (player != null)
+        {
+            player.PushFrictionCoefficient(m_coefficient);
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider col)
+    {
+        PlayerMovementController player = col.GetComponent<PlayerMovementController>();
+        if (player != null)
+        {
+            player.PopFrictionCoefficient();
+        }
+    }
+}
