bd46cd5c426f33e5ab6adae7291b86945bca35a3
ribazmaj <fistric.tomislav.uk@gmail.com>
2021-01-30 19:42:00 +0000

    Animator controler and state changes

--- Assets/Script/Animation/PlayerAnimController.cs
+++ Assets/Script/Animation/PlayerAnimController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerAnimController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    PlayerMovementController m_movementController = null;
+
+    [SerializeField]
+    Animator m_animator = null;
+
+    //m_animator = GetComponent<Animator>();
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_animator.SetFloat("CurrentSpeed", m_movementController.CurrentSpeed);
+
+        // m_animator.SetBool("DiggingRightNow", m_diddgingController.Digging);
+       
+        // m_animator.SetTrigger("Dig", m_diggingController.Dig);
+
+        // if (test dig = true) { testdig = false} m_animator.SetTrigget.Dig;
+
+
+        /*
+        if (Input.GetKey("w"))
+        {
+            m_animator.SetBool;
+        }
+
+        if (Input.GetKey("space"))
+        {
+            m_animator.SetBool "DigggingRightNow" = false;
+        }
+        */
     }
+
+    
 }
