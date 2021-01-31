1fb8815a48f4b74683a33db361c3a74ee96ac367
wallmasterr <lowtek@hotmail.co.uk>
2021-01-31 00:17:05 +0000

    added main menu

---
+++ Assets/Script/playerAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    PlayerMovementController m_movementController = null;
+
+    [SerializeField]
+    Animator m_animator = null;
+
+    void Start()
+    {
+        //m_animator()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
